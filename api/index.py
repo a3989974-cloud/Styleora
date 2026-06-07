2312acd14803ffa57074e1d6cd41dd315df50bde
ali <a3989974@gmail.com>
2026-06-07 21:17:51 +0500

    Use direct migration executor in api/index.py

--- api/index.py
+++ api/index.py
@@ -6,10 +6,14 @@ sys.path.insert(0, os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'luxe_store.settings')
 
 import django
-from django.core.management import call_command
-
 django.setup()
-call_command('migrate', '--noinput')
+
+from django.db import connections
+from django.db.migrations.executor import MigrationExecutor
+
+connection = connections['default']
+executor = MigrationExecutor(connection)
+executor.migrate(executor.loader.graph.leaf_nodes())
 
 from luxe_store.wsgi import application
 app = application
