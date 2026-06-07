cbd7e652ae29cb515d9a1cf849c1835c7e706142
ali <a3989974@gmail.com>
2026-06-07 21:11:19 +0500

    Auto-run migrations on Vercel startup

--- api/index.py
+++ api/index.py
@@ -5,5 +5,11 @@ sys.path.insert(0, os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'luxe_store.settings')
 
+import django
+from django.core.management import call_command
+
+django.setup()
+call_command('migrate', '--noinput')
+
 from luxe_store.wsgi import application
 app = application
